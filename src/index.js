2d7b83e277db712342f2cff1fc0b045dfb7e7193
lisandroseia <lisandroseia11@gmail.com>
2022-02-01 11:21:27 -0300

    Fetch url

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './style.css';
+
+const url = 'www.themealdb.com/api/json/v1/1/random.php'
+
+async function logFetch(){
+   const response = await fetch(url)
+   const data = await response.json()
+   console.log(data)
+}
+
+logFetch();
+
+
