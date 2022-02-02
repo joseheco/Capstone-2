0dedcbf593aa2250874cce02d3327125da020233
Jose Herrera <Heco@Alejandras-MacBook-Pro.local>
2022-02-02 18:52:45 -0500

    fix index.js conflicts

--- src/index.js
+++ src/index.js
@@ -20,9 +20,6 @@ async function display(){
    const meals = await  getObj;
    const displaySection = document.querySelector('.meals')
 
-   const overflo = document.querySelector('.container-meal')
-overflo.classList.remove('container-meal');
-
    for(let i = 0; i < meals.length; i+=1){
 
      const card = document.createElement('div');
@@ -43,24 +40,8 @@ overflo.classList.remove('container-meal');
    }
 }
 
-     const card = document.createElement('div');
-
-     card.classList.add('card');
-     card.innerHTML = `<img src="${meals[i].strMealThumb}">
-                       <div class="cardTitle">
-                       <h1>${meals[i].strMeal}</h1>
-                       <box-icon data-value="${meals[i].idMeal}" class="heart" name='heart'></box-icon>
-                       </div>
-                       <p data-value="${meals[i].idMeal}" class="likes"></p>
-                       <div class="btns">
-                       <button data-value="${meals[i].idMeal}" type="button" class="comment-btn">Comments</button>
-                       </div>
-     `
-     
-     displaySection.appendChild(card)
-   }
-}
-
+const overflo = document.querySelector('.container-meal')
+overflo.classList.remove('container-meal');
 const commentBtnEvent = async () => {
    await display();
    const commentBtns = document.querySelectorAll('.comment-btn');
@@ -89,8 +70,6 @@ async function displayLikes(){
    [0].likes)
      
    })
-
-
 
 
 // add comment to api //
