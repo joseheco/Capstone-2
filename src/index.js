da7a81751274b49515c845951e1412a7228088fe
lisandroseia <lisandroseia11@gmail.com>
2022-02-03 12:20:49 -0300

    Fix linters and add home page counter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import './style.css';
 import 'boxicons';
 
-import displayLikes from './modules/likes';
-import commentBtnEvent from './modules/commectBtn';
+import displayLikes from './modules/likes.js';
+import commentBtnEvent from './modules/commectBtn.js';
+import homeCounter from './modules/homeCounter.js';
 
 commentBtnEvent();
-
-
-
-
 displayLikes();
+homeCounter();
